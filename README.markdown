72d57d30d4ea97f7f159e03705c2d5b56649feab
Dan Poltawski <talktodan@gmail.com>
2012-04-02 21:34:45 +0800

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,19 @@
+Moodle userscripts
+==================
+
+Here are some scripts that might be userful to other moodler.
+
+- Pull Request Helper - a copy and paste helper for Moodle integrators [INSTALL](http://github.com/danpoltawski/userscripts-moodle/raw/master/pull-request-helper.user.js)
+
+INSTALL
+-------
+
+*Google Chrome* - Just click the link to install.
+
+*Firefox* - You will need to install the greasemonkey extension then click the link
+
+LICENSE
+-------
+This work is released under the terms of the GNU General Public License, v3 or later.
+
+http://www.gnu.org/copyleft/gpl.html
---
+++ pull-request-helper.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name          Pull Request Helper
+// @description   Makes copy and paste easier for Moodle integrators
+// @author        Dan Poltawski
+// @homepage      http://github.com/danpoltawski/userscripts-moodle
+// @namespace     http://userscripts.danpoltawski.co.uk
+// @match         http*://tracker.moodle.org/browse/*
+// ==/UserScript==
+
+var GITREPO = document.getElementById('customfield_10100-val');
+
+if (!GITREPO) {
+    return;
+}
+
+var MASTER = document.getElementById('customfield_10111-val');
+var MOODLE_22_STABLE = document.getElementById('customfield_10711-val');
+var MOODLE_21_STABLE = document.getElementById('customfield_10311-val');
+var MOODLE_20_STABLE = document.getElementById('customfield_10113-val');
+var MOODLE_19_STABLE = document.getElementById('customfield_10116-val');
+
+var parent = document.getElementById('customfieldmodule');
+
+var s= '<p>Integrators Cheat Sheet:</p><pre>';
+
+if (MASTER) {
+    s+= "git checkout master\n";
+    s+= 'git pull ' + GITREPO.innerText + ' ' + MASTER.innerText + "\n\n";
+}
+
+if (MOODLE_22_STABLE) {
+    s+= "git checkout MOODLE_22_STABLE\n";
+    s+= 'git pull ' + GITREPO.innerText + ' ' + MOODLE_22_STABLE.innerText + "\n\n";
+}
+
+if (MOODLE_21_STABLE) {
+    s+= "git checkout MOODLE_21_STABLE\n";
+    s+= 'git pull ' + GITREPO.innerText + ' ' + MOODLE_21_STABLE.innerText + "\n\n";
+}
+
+if (MOODLE_20_STABLE) {
+    s+= "git checkout MOODLE_20_STABLE\n";
+    s+= 'git pull ' + GITREPO.innerText + ' ' + MOODLE_20_STABLE.innerText + "\n\n";
+}
+
+if (MOODLE_19_STABLE) {
+    s+= "git checkout MOODLE_19_STABLE\n";
+    s+= 'git pull ' + GITREPO.innerText + ' ' + MOODLE_19_STABLE.innerText + "\n\n";
+}
+
+// prepare div
+var integratorDiv = document.createElement('div');
+integratorDiv.innerHTML = s;
+integratorDiv.setAttribute('style', 'border-top: 1px solid #DDD;');
+
+parent.insertBefore(integratorDiv, document.getElementById('tabCellPane1'));
