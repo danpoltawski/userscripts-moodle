5b2e62baab37426662db9f8d33bac71fb3d0cb45
Dan Poltawski <talktodan@gmail.com>
2012-07-12 13:13:37 +0800

    Support 23_STABLE on userscript

--- pull-request-helper.user.js
+++ pull-request-helper.user.js
@@ -16,6 +16,7 @@ if (!GITREPO) {
 }
 
 var MASTER = document.getElementById('customfield_10111-val');
+var MOODLE_23_STABLE = document.getElementById('customfield_11016-val');
 var MOODLE_22_STABLE = document.getElementById('customfield_10711-val');
 var MOODLE_21_STABLE = document.getElementById('customfield_10311-val');
 var MOODLE_20_STABLE = document.getElementById('customfield_10113-val');
@@ -28,6 +29,11 @@ var s= '<p>Integrators Cheat Sheet:</p><pre>';
 if (MASTER) {
     s+= "git checkout master\n";
     s+= 'git pull ' + GITREPO.innerText + ' ' + MASTER.innerText + "\n\n";
+}
+
+if (MOODLE_23_STABLE) {
+    s+= "git checkout MOODLE_23_STABLE\n";
+    s+= 'git pull ' + GITREPO.innerText + ' ' + MOODLE_23_STABLE.innerText + "\n\n";
 }
 
 if (MOODLE_22_STABLE) {
