521638b216852aee4c54d5c56d80d57fb7f980be
Frederic Massart <fred@moodle.com>
2012-10-19 14:24:08 +0800

    Toggle the Moodle.org menu on Moodle Tracker

---
+++ tracker-toggle-moodle-menu.user.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name        Tracker Toggle Moodle Menu
+// @description Toggle the Moodle.org menu on the Tracker.
+// @include     http://tracker.moodle.org/*
+// @include     https://tracker.moodle.org/*
+// @match       http://tracker.moodle.org/*
+// @match       https://tracker.moodle.org/*
+// @grant       none
+// @author      Frédéric Massart - FMCorz.net
+// @version     0.19
+// ==/UserScript==
+
+// Settings.
+var settings = {
+    hidebydefault: true
+};
+
+// Limit to Jira, shoudln't be necessary.
+if (document.body.id == 'jira') {
+    var toggle_object = {
+        menuid: 'moodle-menu',
+        navid: 'main-nav',
+        menu: null,
+        nav: null,
+        init: function() {
+            this.menu = document.getElementById(this.menuid);
+            this.nav = document.getElementById(this.navid);
+            if (!this.menu) {
+                return;
+            }
+            if (settings.hidebydefault) {
+                this.hide();
+            }
+            if (this.nav) {
+                this.add_toggle_button();
+            }
+            return this;
+        },
+        add_toggle_button: function() {
+            var btn = document.createElement('li');
+            btn.className = 'aui-dd-parent nonlazy';
+            var a = document.createElement('a');
+            a.textContent = 'Toggle menu';
+            a.className = 'lnk';
+            a.onclick = this.toggle;
+            btn.appendChild(a);
+            this.nav.appendChild(btn);
+        },
+        hide: function() {
+            toggle_object.menu.style.display = 'none';
+        },
+        show: function() {
+            toggle_object.menu.style.display = 'block';
+        },
+        toggle: function() {
+            if (toggle_object.menu.style.display != 'block') {
+                toggle_object.show();
+            } else {
+                toggle_object.hide();
+            }
+            return false;
+        }
+    };
+    t = toggle_object.init();
+}
